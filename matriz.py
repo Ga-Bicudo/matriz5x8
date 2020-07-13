fdc327dcbfe2d0d55cf7268d43e08900eb630372
Gabriel Bicudo Rosa <gabrielbicudorosa@gmail.com>
2020-07-13 15:30:34 -0300

    somente matriz feita

---
+++ matriz.py
@@ -0,0 +1,26 @@
+import numpy 
+
+matriz = [ ]
+
+linhas = int(input("escolha quantas linhas tera a matriz:"))
+colunas = int(input("escolha quantas colunas tera a matriz:"))
+for l in range(linhas):
+    i = []
+    for c in range(colunas):
+        i.append('x')
+    matriz.append(i)    
+
+for x in matriz:    
+    print(matriz[2])                 
+
+'''def cria_blocos(altura: int, largura: int):
+    largura = input("insira a altura do bloco(de 0 a 8")
+    altura = input("insira a largura do bloco(de 0 a 5")'''
+
+
+
+
+
+
+
+       
