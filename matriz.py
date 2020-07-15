3ed1ec19b587c65eb73280d4aab51753207856ac
Gabriel Bicudo Rosa <gabrielbicudorosa@gmail.com>
2020-07-14 22:13:53 -0300

    subindo metodo para calcular se os blocos entram na matriz

--- matriz.py
+++ matriz.py
@@ -1,30 +1,43 @@
-import numpy 
+import numpy #ainda não usei mas há possibilidade.
 
 matriz = [ ]
-mataux = [ ]
 
 linhas = int(input("escolha quantas linhas tera a matriz:"))
 colunas = int(input("escolha quantas colunas tera a matriz:"))
+area_matriz = linhas * colunas
 for l in range(linhas):
         i = []
         for c in range(colunas):
             i.append('0')
         matriz.append(i)    
 
-
-largura = int(input("insira a altura do bloco:"))
-altura = int(input("insira a largura do bloco:"))
+blocks = []  #lista criada para guardar os blocos
+area_blocos = 0
+for blocos in range(0,area_matriz >= area_blocos): #metodo para checar se os blocos encaixaram na matriz.
+    largura = int(input("insira a altura do bloco:"))
+    altura = int(input("insira a largura do bloco:"))
+    bloco = largura * altura   # criando objeto bloco
+    blocks.append(bloco)      #adicionalndo na lista blocks o objeto
+    if altura > linhas or largura > colunas:  #validando se o objeto pode ser guardado
+        print("bloco inválido")
+        continue
+    else:
+        for x in (blocks):
+            print(x)
+            
+    
  
-if altura > linhas or largura > colunas:
-    print("bloco inválido")
-else:
-    for d in range(altura):
-        for z in range(largura):    
-            matriz[d][z] = 'x'
+   
+    """elif():
+        for d in range(altura):
+            for z in range(largura):    
+                matriz[d][z] = 'x'"""
 
 
 for x in matriz:
     print(x)    
+
+
                      
 
                     
